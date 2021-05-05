1f2c023351e0548ed92c83c1fdd38d00bd23db11
Sergey Malyshev <malyshevs_v@mail.ru>
2021-05-06 02:34:48 +0300

    add TestBean, TestSpring and ApplicationContext.xml

---
+++ src/main/java/ru/smal/TestBean.java
@@ -0,0 +1,13 @@
+package ru.smal;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class TestBean {
+    private String name;
+
+}
---
+++ src/main/java/ru/smal/TestSpring.java
@@ -0,0 +1,14 @@
+package ru.smal;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args){
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean);
+    }
+}
