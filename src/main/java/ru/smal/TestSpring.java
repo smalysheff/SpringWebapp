cc618332ac8aae86717c607eb4ed9a7b08c338d0
Sergey Malyshev <malyshevs_v@mail.ru>
2021-05-07 00:43:59 +0300

    dependency injection 1

--- src/main/java/ru/smal/TestSpring.java
+++ src/main/java/ru/smal/TestSpring.java
@@ -7,10 +7,7 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
