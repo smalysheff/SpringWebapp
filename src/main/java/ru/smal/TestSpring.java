a5e0f02faee11c4aaebe57fcc085fbb964e36b39
Sergey Malyshev <malyshevs_v@mail.ru>
2021-05-08 01:57:29 +0300

    scope prototype singletone

--- src/main/java/ru/smal/TestSpring.java
+++ src/main/java/ru/smal/TestSpring.java
@@ -4,13 +4,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args){
+
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(firstMusicPlayer.hashCode());
+        System.out.println(secondMusicPlayer.hashCode());
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+
 
         context.close();
     }
